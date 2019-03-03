665798df263dcc0cdc04165854c8ec5cb9ec4b05
Ju An Oh <juan@D-10-19-122-161.dhcp4.washington.edu>
2019-03-03 11:49:07 -0800

    initial setting

---
+++ doggieranker/doggieranker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  doggieranker
+//
+//  Created by Ju An Oh on 3/3/19.
+//  Copyright © 2019 Ju An Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
